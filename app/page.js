99d604958ffa7ddf0ed5fc68f43a6e1794cb0efb
mahamatoumaroumate <mhtoumate1999@gmail.com>
2024-04-23 21:05:39 +0300

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Navbar from '@/component/Navbar'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Navbar />
+    </div>
+  )
+}
+export default page
---
+++ component/Navbar.jsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <div className='grid grid-cols-3'>
+      <p className='text-primary'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        sequi, sed ad id placeat, doloribus non, saepe alias fugit beatae
+        temporibus. Nesciunt magni quis porro iure unde dignissimos qui dicta?
+      </p>
+      <p className='text-secondary'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+        excepturi consequatur minus, ullam odio deserunt mollitia? Cum, corporis
+        dolor explicabo, vel libero tempora ad ex ipsum quae debitis delectus
+        impedit!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+        iusto? Vel aliquam, incidunt doloribus debitis harum eligendi quas
+        recusandae reiciendis id dolores nulla ipsa, veniam atque sunt minus
+        illum enim.
+      </p>
+    </div>
+  )
+}
+export default Navbar
